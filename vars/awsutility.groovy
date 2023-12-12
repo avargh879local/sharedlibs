ee2f95cfe29e803d63d7c9329b2d1452d00dc4b7
avargh879local <134178695+avargh879local@users.noreply.github.com>
2023-12-12 09:08:17 -0600

    Update awsutility.groovy

--- vars/awsutility.groovy
+++ vars/awsutility.groovy
@@ -3,12 +3,12 @@ import hudson.util.Secret
 
 // Function to run 'aws --version'
 def getAWSVersion(credentialsId) {
-    def version = sh(script: "${awsCliPath} --version", returnStatus: true, credentialsId: credentialsId).trim()
+    def version = sh(script: "aws --version", returnStatus: true, credentialsId: credentialsId).trim()
     return version
 }
 
 // Function to get the number of ECS clusters running
 def getECSClusterCount(credentialsId) {
-    def clusterCount = sh(script: "${awsCliPath} ecs list-clusters --output json | jq '.clusterArns | length'", returnStatus: true, credentialsId: credentialsId).trim()
+    def clusterCount = sh(script: "aws ecs list-clusters --output json | jq '.clusterArns | length'", returnStatus: true, credentialsId: credentialsId).trim()
     return Integer.parseInt(clusterCount)
 }
