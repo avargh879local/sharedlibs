dc181aa0e61a67f43ef17d3b336c92eb9e484707
avargh879local <134178695+avargh879local@users.noreply.github.com>
2023-12-12 11:14:04 -0600

    Update awsutility.groovy

--- vars/awsutility.groovy
+++ vars/awsutility.groovy
@@ -7,20 +7,20 @@ def getAWSVersion() {
 }
 
 def countActiveEC2Instances(String region) {
-    // AWS CLI command to describe instances in the specified region
-    // Filtering for instances with instance-state-name 'running' to consider only active instances
     def command = "aws ec2 describe-instances --region ${region} --query 'Reservations[*].Instances[?State.Name==`running`]' --output json"
-
-    // Execute the command and capture the output
     def output = sh(script: command, returnStdout: true).trim()
 
-    // Parse the JSON output to extract instance information
     def activeInstancesCount = 0
     def parsedOutput = new groovy.json.JsonSlurper().parseText(output)
+
     parsedOutput.each { reservation ->
-        // Each reservation is an array of instances
-        activeInstancesCount += reservation.size()
+        reservation.each { instance ->
+            if (instance.State.Name == 'running') {
+                activeInstancesCount++
+            }
+        }
     }
 
     return activeInstancesCount
 }
+
