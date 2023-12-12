1ef9a238e99f80d450e03115ac9c499ffdf544bf
avargh879local <134178695+avargh879local@users.noreply.github.com>
2023-12-12 10:54:11 -0600

    Update awsutility.groovy

--- vars/awsutility.groovy
+++ vars/awsutility.groovy
@@ -6,38 +6,20 @@ def getAWSVersion() {
     return version
 }
 
-def listEC2Instances(String region) {
-    def command = "aws ec2 describe-instances --region ${region} --query 'Reservations[*].Instances[*].[InstanceId, PublicIpAddress]' --output json"
+def countActiveEC2Instances(String region) {
+    // AWS CLI command to describe instances in the specified region
+    // Filtering for instances with instance-state-name 'running' to consider only active instances
+    def command = "aws ec2 describe-instances --region ${region} --query 'Reservations[*].Instances[?State.Name==`running`]' --output json"
+
+    // Execute the command and capture the output
     def output = sh(script: command, returnStdout: true).trim()
 
+    // Parse the JSON output to extract instance information
+    def activeInstancesCount = 0
     def parsedOutput = new groovy.json.JsonSlurper().parseText(output)
-    def instanceDescriptions = []
-    int count = 1
-
     parsedOutput.each { reservation ->
-        reservation.each { instance ->
-            if (instance[0] != null && instance[1] != null) {
-                def description = "${ordinal(count)} instance: ID - ${instance[0]}, IP - ${instance[1]}"
-                instanceDescriptions << description
-                count++
-            }
-        }
+        activeInstancesCount += reservation.size()
     }
 
-    return instanceDescriptions
+    return activeInstancesCount
 }
-
-// Helper method to convert numbers to ordinal format (1st, 2nd, 3rd, etc.)
-def ordinal(int number) {
-    def suffixes = ['th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th']
-    switch (number % 100) {
-        case 11:
-        case 12:
-        case 13:
-            return number + 'th'
-        default:
-            return number + suffixes[number % 10]
-    }
-}
-
-
