17b0e557a5a259f76aa7ee2fe3f3c2f373bd153b
avargh879local <134178695+avargh879local@users.noreply.github.com>
2023-12-12 10:06:57 -0600

    Update awsutility.groovy

--- vars/awsutility.groovy
+++ vars/awsutility.groovy
@@ -1,6 +1,5 @@
 import hudson.util.Secret
 
-
 // Function to run 'aws --version'
 def getAWSVersion(credentialsId) {
     def version = sh(script: "aws --version", returnStdout: true, credentialsId: credentialsId).trim()
@@ -9,15 +8,7 @@ def getAWSVersion(credentialsId) {
 
 // Function to get the number of ECS clusters running
 def getECSClusterCount(credentialsId) {
-    def clusterCountOutput = sh(script: "aws ecs list-clusters", returnStdout: true, credentialsId: credentialsId).trim()
-    return clusterCountOutput
+    def clusterCountOutput = sh(script: "aws ecs list-clusters --output json | jq '.clusterArns | length'", returnStdout: true, credentialsId: credentialsId).trim()
+        return clusterCountOutput
+    }
 }
-
-// def getEC2InstancesInSubnet(region, subnetId) {
-//     // Run AWS CLI command to describe EC2 instances in the specified region and subnet
-//     def instancesOutput = sh(script: "${awsCliPath} ec2 describe-instances --region ${region} --filters Name=subnet-id,Values=${subnetId} --query 'Reservations[].Instances[] | {InstanceId: InstanceId, PublicIpAddress: PublicIpAddress}' --output json", returnStdout: true).trim()
-
-//     return instancesOutput
-// }
-
-
