db56219c821028b88aa0ebaefaab3aa7f9f731fc
avargh879local <134178695+avargh879local@users.noreply.github.com>
2023-12-12 09:36:20 -0600

    Update awsutility.groovy

--- vars/awsutility.groovy
+++ vars/awsutility.groovy
@@ -3,13 +3,13 @@ import hudson.util.Secret
 
 // Function to run 'aws --version'
 def getAWSVersion(credentialsId) {
-    def version = sh(script: "aws --version", returnStdout: true, credentialsId: abyscred).trim()
+    def version = sh(script: "aws --version", returnStdout: true, credentialsId: credentialsId).trim()
     return version
 }
 
 // Function to get the number of ECS clusters running
 def getECSClusterCount(credentialsId) {
-    def clusterCountOutput = sh(script: "aws ecs list-clusters --output json | jq '.clusterArns | length'", returnStdout: true, credentialsId: abyscred).trim()
+    def clusterCountOutput = sh(script: "aws ecs list-clusters --output json | jq '.clusterArns | length'", returnStdout: true, credentialsId: credentialsId).trim()
     try {
         return Integer.parseInt(clusterCountOutput)
     } catch (NumberFormatException e) {
