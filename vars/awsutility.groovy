cf06485cdd9f95e3453a358448a2dd3ddc6e86d9
avargh879local <134178695+avargh879local@users.noreply.github.com>
2023-12-12 09:09:51 -0600

    Update awsutility.groovy

--- vars/awsutility.groovy
+++ vars/awsutility.groovy
@@ -1,14 +1,19 @@
-// Import the necessary classes
 import hudson.util.Secret
 
 // Function to run 'aws --version'
 def getAWSVersion(credentialsId) {
-    def version = sh(script: "aws --version", returnStatus: true, credentialsId: credentialsId).trim()
+    def version = sh(script: "aws --version", returnStdout: true, credentialsId: credentialsId).trim()
     return version
 }
 
 // Function to get the number of ECS clusters running
 def getECSClusterCount(credentialsId) {
-    def clusterCount = sh(script: "aws ecs list-clusters --output json | jq '.clusterArns | length'", returnStatus: true, credentialsId: credentialsId).trim()
-    return Integer.parseInt(clusterCount)
+    def clusterCountOutput = sh(script: "aws ecs list-clusters --output json | jq '.clusterArns | length'", returnStdout: true, credentialsId: credentialsId).trim()
+    try {
+        return Integer.parseInt(clusterCountOutput)
+    } catch (NumberFormatException e) {
+        // Handle parse error
+        println("Error parsing cluster count: ${e.message}")
+        return -1  // or other error indication
+    }
 }
