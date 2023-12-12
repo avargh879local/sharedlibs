b55c41b72b652b955519f38e86efd8575f60709f
avargh879local <134178695+avargh879local@users.noreply.github.com>
2023-12-12 10:01:36 -0600

    Update awsutility.groovy

--- vars/awsutility.groovy
+++ vars/awsutility.groovy
@@ -10,10 +10,14 @@ def getAWSVersion(credentialsId) {
 // Function to get the number of ECS clusters running
 def getECSClusterCount(credentialsId) {
     def clusterCountOutput = sh(script: "aws ecs list-clusters", returnStdout: true, credentialsId: credentialsId).trim()
-    // if(clusterCountOutput == "0" || clusterCountOutput == "null"){
-    //     println ("There are no active clusters")
-    // }else{
-    //     println ("clusters are present this is the present value : ${clusterCountOutput}")
     return clusterCountOutput
 }
+
+// def getEC2InstancesInSubnet(region, subnetId) {
+//     // Run AWS CLI command to describe EC2 instances in the specified region and subnet
+//     def instancesOutput = sh(script: "${awsCliPath} ec2 describe-instances --region ${region} --filters Name=subnet-id,Values=${subnetId} --query 'Reservations[].Instances[] | {InstanceId: InstanceId, PublicIpAddress: PublicIpAddress}' --output json", returnStdout: true).trim()
+
+//     return instancesOutput
+// }
+
 
